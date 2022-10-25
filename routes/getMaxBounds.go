9d92d19dbe23833e265ec2de13b114f88d3a629e
Gregory Thomas Keller <kelle200@data.cs.purdue.edu>
2022-10-25 16:24:39 -0400

    Week 2 starting to add endpoint to update ride info

---
+++ routes/getMaxBounds.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+type getEtaBody struct {
+	RideID string `json:"ride_id"`
+}
+
+func getEta(e *core.ServeEvent, app *pocketbase.PocketBase) error {
+	e.Router.AddRoute(echo.Route{
+		Method: http.MethodPost,
+		Path:   "/api/getEta",
+		Handler: func(c echo.Context) error {
+			var body getEtaBody
+			c.Bind(&body)
+
+			rides, _ := app.Dao().FindCollectionByNameOrId("rides")
+			ride, _ := app.Dao().FindFirstRecordByData(rides, "id", body.RideID)
+
+			if ride != nil {
+				return c.JSON(200, map[string]interface{}{"eta": ride.GetDataValue("eta")})
+			}
+
+			return c.NoContent(400)
+		},
+		Middlewares: []echo.MiddlewareFunc{
+			apis.RequireGuestOnly(),
+		},
+	})
+
+	return nil
+}
