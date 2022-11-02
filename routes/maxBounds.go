e7aa0f37b8a0440909de4f50ddc3c013532e378f
Gregory Thomas Keller <kelle200@data.cs.purdue.edu>
2022-11-02 10:51:41 -0400

    endpoint for recieving rider info during ride

--- routes/maxBounds.go
+++ routes/maxBounds.go
@@ -21,5 +21,26 @@ func getEta(e *core.ServeEvent, app *pocketbase.PocketBase) error {
 		
 	})
 
+	Handler: func(c echo.Context) error {
+		var body getMaxBounds
+		c.Bind(&body)
+
+		events, _ := app.Dao().FindCollectionByNameOrId("events")
+		event, _ := app.Dao().FindFirstRecordByData(events, "event_id", body.EventID)
+	
+
+		if event != nil {
+			return c.JSON(200, map[string]interface{}{
+				"address": event.GetDataValue("address")
+				"maxBounds": event.GetDataValue("ride_max_radius")
+			})
+		}
+
+		return c.NoContent(400)
+	},
+	Middlewares: []echo.MiddlewareFunc{
+		apis.RequireGuestOnly(),
+	},
+
 	return nil
 }
